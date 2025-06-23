4e5f91008bb54e14eeac2ac910dbc27bdfe8e59d
Thais Duarte <thais_duarte03@outlook.com>
2025-06-23 20:52:09 -0300

    outras paginas

--- src/DBLP/robdepts.py
+++ src/DBLP/robdepts.py
@@ -14,11 +14,11 @@ ranking = {}
 num_prof_list = []
 area_list = []
 
-reader1 = csv.reader(open("research-areas-config.csv", 'r'))
+reader1 = csv.reader(open("../../data/configs/research-areas-config.csv", 'r'))
 for a in reader1:
     area_list.append(a[0])
 
-reader3 = csv.reader(open("profs.csv", 'r'))
+reader3 = csv.reader(open("../../data/configs/profs/profs.csv", 'r'))
 for a in reader3:
     num_prof_list.append(a[1])
 
@@ -28,7 +28,7 @@ for area in area_list:
     for dept_tuple in reader2:
         dept = dept_tuple[0]
         if not(dept in dept_list):
-        dept_list.append(dept)
+            dept_list.append(dept)
         s = float(dept_tuple[1])
         score[dept] = s
     ranking[area] = score
@@ -41,7 +41,7 @@ f2.write('<ul>\n')
 for dept in dept_list:
     num_prof = num_prof_list.count(dept)
     if num_prof < 10:
-    continue
+        continue
     dept2 = dept.replace("/", "").replace(" ","").lower()
     f2.write('<li>  <a href="https://csindexbr.org/depts.html?d=' + dept2 + '">' + dept + '</a>\n')
     f = open('depts/scores-' + dept2 + '.csv','w')
@@ -49,9 +49,9 @@ for dept in dept_list:
         f.write(area.upper())
         f.write(',')
         if dept in ranking[area]:
-        f.write(str(ranking[area][dept]))
+            f.write(str(ranking[area][dept]))
         else:
-        f.write('0.00')
+            f.write('0.00')
         f.write('\n')
     f.close()
 
--- src/DBLP/runprofs.py
+++ src/DBLP/runprofs.py
@@ -2,46 +2,62 @@ import csv
 import glob
 import os
 
+# recebe o caminho do arquivo e retorna o numero de linhas
 def file_size(file):
     num_lines = sum(1 for line in open(file))
     return num_lines
 
+# para cada professor, verifica o tamnho dos arquivos encontrados
+    # determina a area conforme o arquivo com maior numero de lionhas
 def get_area(prof):
     size = 0
     area = ""
-    for file in glob.glob("../cache/profs/*" + prof + "-papers.csv"):
+    for file in glob.glob("../cache/profs/papers/*" + prof + "-papers.csv"):
         size2 = file_size(file)
         if size2 > size:
             size = size2
             area = file[15:file.index('-')]
     return area.upper()
 
+
+# Função que cria o arquivo HTML individual de um professor, se ainda não existir
+# Esse HTML é construído combinando os arquivos _authors1.html e _authors2.html
 def create_p_file(prof_name):
-    file_name = "../p/" + prof_name + '.html'
+    file_name = "../src/p/" + prof_name + '.html'
     if os.path.exists(file_name):
         return
     line = '      var corebr_author = "' + prof_name + '"\n'
     out = open(file_name, 'w')
-    file1 = open('../authors1.html', 'r')
-    file2 = open('../authors2.html', 'r')
+    file1 = open('../src/_authors1.html', 'r')
+    # src\utils\_authors1.html
+    file2 = open('../src/_authors2.html', 'r')
     out.write(file1.read())
     out.write(line)
     out.write(file2.read())
     out.close
 
+# Dicionário para armazenar a afiliação institucional de cada professor
 inst = {}
 reader1 = csv.reader(open("../../data/configs/all-researchers.csv", 'r'))
 
+# Preenche o dicionário inst com nome do professor e sua instituição
 for p in reader1:
     prof = p[0]
     dept = p[1]
     inst[prof] = dept
 
+# Abre o arquivo onde será gerado o HTML com a lista de professores
 out = open('../profs.html','a')
+# Cria arquivo CSV com as informações dos professores (nome, instituição, área)
 out2 = open('../../data/configs/profs/profs.csv','w')
+
 # out3 = open('../html/sitemap_p.txt','w')
+
+# Leitura dos nomes em all-authors.csv
 reader2 = csv.reader(open("../../data/configs/profs/all-authors.csv", 'r'))
 
+
+# Para cada professor listado em all-authors, escreve seu link em profs.html e registra info em profs.csv
 for p in reader2:
     prof = p[0]
     p2 = prof.replace(" ", "-")
